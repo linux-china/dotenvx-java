d5f3964e30b3ad01f5a6cfcc330dfe6bdcb95204
Libing Chen <libing.chen@gmail.com>
2025-08-07 11:37:42 +0800

    test: add testDotenvx()

--- src/test/java/io/github/cdimascio/dotenv/DotenvxTest.java
+++ src/test/java/io/github/cdimascio/dotenv/DotenvxTest.java
@@ -12,4 +12,15 @@ public class DotenvxTest {
         final String hello = dotenv.get("HELLO");
         assertThat(hello).isEqualTo("World");
     }
+
+    @Test
+    public void testDotenvx() {
+        DotenvxBuilder builder = Dotenvx.configure()
+                .filename(".env")
+                .ignoreIfMissing()
+                .systemProperties();
+        Dotenv dotenv = builder.load();
+        final String hello = dotenv.get("HELLO");
+        System.out.println("HELLO: " + hello);
+    }
 }
