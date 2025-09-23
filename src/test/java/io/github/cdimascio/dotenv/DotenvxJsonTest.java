e6f9aea98e4eb031eef7c7d0252550f53b320926
Libing Chen <libing.chen@gmail.com>
2025-09-23 12:17:43 +0800

    chore: polishing

--- src/test/java/io/github/cdimascio/dotenv/DotenvxJsonTest.java
+++ src/test/java/io/github/cdimascio/dotenv/DotenvxJsonTest.java
@@ -1,9 +1,9 @@
 package io.github.cdimascio.dotenv;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import io.github.cdimascio.json.DotenvxGlobalJsonSerializer;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,14 +12,13 @@ import java.util.Map;
 
 
 public class DotenvxJsonTest {
-    private static ObjectMapper objectMapper = new ObjectMapper();
+    private static ObjectMapper objectMapper = getDotenvxObjectMapper();
 
-    @BeforeAll
-    static void setup() throws Exception {
+    static ObjectMapper getDotenvxObjectMapper() {
         SimpleModule simpleModule = new SimpleModule();
         simpleModule.addSerializer(new DotenvxGlobalJsonSerializer("02b4972559803fa3c2464e93858f80c3a4c86f046f725329f8975e007b393dc4f0"));
         //simpleModule.addDeserializer(String.class, new DotenvxGlobalJsonDeserializer(privateKey));
-        objectMapper.registerModule(simpleModule);
+        return JsonMapper.builder().addModules(simpleModule).build();
     }
 
     @Test
